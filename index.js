6006df626fb46f655991c9846002515cf7758b51
rahmatullah23 <tomalg@gmail.com>
2020-10-08 16:14:43 +0300

    final

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
 const port = 4030
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tnjvj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
